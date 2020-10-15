3b67a0c3147871d8a763adc0008f8abf2f7d68e1
Johanna Simfors <jsimfors@gmail.com>
2020-10-15 16:45:51 +0200

    images added

--- src/pages/hehe/index.js
+++ src/pages/hehe/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import '../../innerPages.css'
 
-
 const Hehepage = () => (
   <div className="content">
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-12">
           <div className="inner-content" id="startpage">
-            <h2>Memes</h2>
+            <img src={require('../../imgs/' + Math.floor(Math.random()*(15)+1) + '.jpg')} />
           </div>        
         </div>
       </div>
