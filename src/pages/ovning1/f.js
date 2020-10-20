10a79baee87890cf6543f0051db62f455f2e3c99
Johanna Simfors <jsimfors@gmail.com>
2020-10-20 14:47:35 +0200

    Copy Update

--- src/pages/ovning1/f.js
+++ src/pages/ovning1/f.js
@@ -12,6 +12,7 @@ const Ovning1f = () => (
             <a href="https://youtu.be/LyGKycYT2v0">Dot products and duality</a>
             <a href="https://youtu.be/eu6i7WJeinw">Cross products</a> Lite överkurs men bra
             av <i>3Blue1Brown</i>.
+            Dock så är den andra videon väldigt baserad på området <i>Linjära Avbildningar</i> som vi börjar med först nästa veckan. Så bli inte orolig om du inte förstår allt i videon. 
             <br/><br/>
             <iframe width="90%" height="1350" title="formelblad" src="https://www.docdroid.net/pGWwrhI/ovning1-teori-pdf" frameBorder="3" allowFullScreen></iframe>
           </div>
--- src/pages/ovning7/f.js
+++ src/pages/ovning7/f.js
@@ -13,8 +13,6 @@ const Ovning7f = () => (
             <a href="https://youtu.be/BaM7OCEm3G0"> Cross products in the light of linear transformations </a>,
             <a href="https://youtu.be/v8VSDg_WQlA"> Nonsquare matrices as transformations between dimensions </a>, och  
             <a href="https://youtu.be/rHLEWRxRGiM"> Three-dimensional linear transformations </a> av <i>3Blue1Brown</i>.
-
-            
             <br/><br/>
             <iframe width="90%" height="1350" title="formelblad" src="https://www.docdroid.net/fgQ4oE1/ovning7-teori-pdf" frameBorder="3" allowFullScreen></iframe>
           </div>
