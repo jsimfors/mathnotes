ebfdb5d24e01ada307808ecc120b218ee29cca48
Johanna Simfors <jsimfors@gmail.com>
2020-12-09 11:50:33 +0100

    New mathmemes added

--- src/pages/hehe/index.js
+++ src/pages/hehe/index.js
@@ -7,7 +7,7 @@ const Hehepage = () => (
       <div className="row">
         <div className="col-md-12">
           <div className="inner-content" id="startpage">
-            <img alt="hehe" src={require('../../imgs/' + Math.floor(Math.random()*(16)+1) + '.jpg')} />
+            <img alt="hehe" src={require('../../imgs/' + Math.floor(Math.random()*(19)+1) + '.jpg')} />
           </div>        
         </div>
       </div>
